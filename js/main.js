d1f17461a871490718e27eb7591ad45a9985cc16
Luana Paes <luanafranca598@gmail.com>
2023-03-23 20:41:22 -0300

    changes

--- js/main.js
+++ js/main.js
@@ -5,3 +5,15 @@ function logar(){
 function comprar(){
     alert('Deseja comprar a peça?')
 }
+
+function comprarPecaFemi(){
+    window.location = "buy-blusa-femi.html"
+}
+
+function comprarPecaMasc(){
+    window.location = "buy-blusa-masc.html"
+}
+
+function comprarPecaInfantil(){
+    window.location = "buy-roupa-inf.html"
+}
